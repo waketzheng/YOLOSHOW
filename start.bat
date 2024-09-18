89079b91f764552dadcd0e5a7b5e33f422da019f
Waket Zheng <waketzheng@gmail.com>
2024-09-18 16:23:49 +0800

    Add windows start script

---
+++ start.bat
@@ -0,0 +1,9 @@
+if exists venv (
+    echo venv exists, skip virtual environment initial.
+) else (
+    echo Create local virtual environment at: venv
+    python -m venv venv --prompt venv3.11-YOLOSHOW --upgrade-deps
+    venv\Scripts\pip install -r requirements.txt
+)
+venv\Scripts\python main.py
+pause
